f156c895499ded1c2702652f4bff85b016045cbe
DimaMalah <89945104+DimaMalah@users.noreply.github.com>
2021-10-16 13:56:08 +0300

    Update script.js
    Исправил, как ты и говорил: создал отдельную функцию создания элемента и кое-какие мелочи подправил)

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const buttonEl = document.querySelector("#btn")
 const dataNameEl = document.querySelector(".enteredname")
 const dataLastnameEl = document.querySelector(".enteredlastname")
 const dataTelEl = document.querySelector(".enteredtel")
-let result = false;
+let validationPass = false;
 
 
 buttonEl.addEventListener("click", getInputData)
@@ -13,26 +13,28 @@ buttonEl.addEventListener("click", getInputData)
 
 function getInputData() {
 	validationName(inputNameEl, inputLastnameEl, inputTelEl)
-	if (result === "true") {
-		dataNameEl.innerHTML = dataNameEl.innerHTML + "<br>" + inputNameEl.value
+	if (validationPass) {
+		dataNameEl.innerHTML = dataNameEl.innerHTML + createElement("div", "data enteredname", inputNameEl.value)
 		inputNameEl.value = "";
-		dataLastnameEl.innerHTML = dataLastnameEl.innerHTML + "<br>" + inputLastnameEl.value
+		dataLastnameEl.innerHTML = dataLastnameEl.innerHTML + createElement("div", "data enteredlastname", inputLastnameEl.value)
 		inputLastnameEl.value = "";
-		dataTelEl.innerHTML = dataTelEl.innerHTML + "<br>" + inputTelEl.value
+		dataTelEl.innerHTML = dataTelEl.innerHTML + createElement("div", "data enteredtel", inputTelEl.value)
 		inputTelEl.value = "";
-		result = false;
+		validationPass = false;
 	} else {
 		alert("Something went wrong! You entered not correct information!")
 		return;
 	}
 }
 
+const createElement = (tag, className, value) =>
+	`<${tag} class="${className}">${value}</${tag}>`;
 
 function validationName(el1, el2, el3) {
 	if (el1.value.trim() && el2.value.trim() &&
 		el3.value.length === 11) {
-		result = "true"
-		console.log(result)
+		validationPass = "true"
+		console.log(validationPass)
 	} else {
 		return console.log(el3.value.length);
 	}
